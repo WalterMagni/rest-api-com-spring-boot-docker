0569de333436f8d0938736a35692c19e1f618669
Walter Magni <Waltermagni@terra.com.br>
2022-06-07 17:06:43 -0300

    Primeiro Commit

---
+++ 00 RestWithSpringBootUdemy Spring Initializer/src/main/java/br/com/walter/restapicomspringbootdocker/RestApiComSpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.walter.restapicomspringbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiComSpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiComSpringBootDockerApplication.class, args);
+	}
+
+}
---
+++ 00 RestWithSpringBootUdemy Spring Initializer/src/test/java/br/com/walter/restapicomspringbootdocker/RestApiComSpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walter.restapicomspringbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiComSpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
